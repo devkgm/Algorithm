04d1ce828f138b5c4a359bca30bc5fb232ba253c
김경모 <gm.kim333@gmail.com>
2024-09-01 22:14:15 +0900

    [Bronze V] Title: N 찍기, Time: 748 ms, Memory: 30640 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int length = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= length; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
